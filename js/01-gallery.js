dde53215cbef794aa0f15a9a072719186bbbdf2b
Larin Artem <larin_art@ukr.net>
2022-04-19 22:58:48 +0300

    task_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const gallery = document.querySelector('.gallery');
+let instance = null;
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link"
+            href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+    }).join('');
+};    
+
+gallery.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+function onImgClick(e) {
+    e.preventDefault(); 
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    instance = basicLightbox.create(`
+    <img
+    class="gallery__image"
+    src = "${e.target.dataset.source}"
+    />
+    `,
+    {
+    onShow: () => {
+        window.addEventListener('keydown', onEscPress);
+    },
+    onClose: () => {
+        window.removeEventListener('keydown', onEscPress);
+    },
+        }
+    );
+    
+instance.show();
+};
+
+gallery.addEventListener('click', onImgClick);
+
+function onEscPress(e){
+    if (e.code === 'Escape') {
+        instance.close();
+    };
+};
